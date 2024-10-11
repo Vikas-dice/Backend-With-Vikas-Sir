0a74c8872b8de0ca00f518bc5054267141bc1993
Vikas Chaudhary DICE <vikas@dice-academy.com>
2024-10-11 13:26:24 +0530

    code pushed for express and middlewares

---
+++ Module 4/index.js
@@ -0,0 +1,127 @@
+const express=require('express')
+const server=express();
+const Exsession=require('express-session')
+
+
+
+//synatx 
+/*
+
+
+server.get("routes","handler")
+
+handler -middleware /function (req res)
+
+
+*/
+
+server.use(function(req,res,next){
+    console.log("hello world")
+  
+    next()
+    
+})
+
+server.use(Exsession({
+    secret: 'some key',
+    resave: false,
+    saveUninitialized: false,
+ 
+}))
+
+
+
+
+server.get('/admin',function(req,res){
+    res.send("server chalu hogya dice academy ")
+    
+
+})
+
+server.get('/edit',function(req,res){
+    res.send("edit data page data")
+})
+
+
+
+
+
+
+server.get('/user',function(req,res){
+    res.send("user data page ")
+})
+
+server.get('/test',function(req,res,next){
+    res.send.vikas=true;
+    res.send("i am done")
+    
+
+})
+
+server.get("*",function(req,res){
+    res.send("no page found")
+})
+
+
+
+
+
+server.listen(3000)
+
+/*middleware -
+
+browser -client - get request - server - {--modification , check}-- middle ware 
+
+middleware - req , res , next
+
+cookies - data brwoser 
+session-data server 
+
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require('express')
+// const app = express()
+// const port = 3000
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
