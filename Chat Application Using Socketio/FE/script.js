d114ae713a87700d315bee50d0eeb48a6c41a46b
Vikas Chaudhary DICE <vikas@dice-academy.com>
2024-12-11 13:37:24 +0530

    chat app day 2

--- Chat Application Using Socketio/FE/script.js
+++ Chat Application Using Socketio/FE/script.js
@@ -1,27 +1,118 @@
+// var socket = io();
+
+// const btn=document.getElementById("btn")
+
+
+// btn.onclick=function(){
+ 
+//         socket.emit("event-from-client")
+//         const div2=document.createElement("div")
+//         div2.innerText="msg coming from CLINET"
+//         document.body.append(div2)
+// }
+
+
+
+
+//listen
+// socket.on('event-from-server',(maal)=>{
+//     console.log(maal)
+
+//     const div =document.createElement("div")
+//     div.innerHTML="some thing coming from server"
+//     document.body.append(div)
+
+// })
+
+// socket.on("vikas-msg-event",(data)=>{
+//     console.log(data)
+
+//     const para=document.createElement("p")
+//     para.innerText="msg coming from vikas side"
+//     document.body.append(para)
+// })
+
+
+
+// const btn2=document.getElementById("btn2")
+// btn2.onclick=function (){
+//     //step 1 - event publish
+//     socket.emit("msg-from-student")
+//     console.log("i am running ")
+//     const div=document.createElement("div")
+//     div.innerText="dice msg"
+//     document.body.append(div)
+
+// }
+
+// io.on('dice-msg-side',(data)=>{
+//     console.log(data)
+
+// })
 var socket = io();
+/*
 
-const btn=document.getElementById("btn")
+2 way comm channel 
 
+vikas ---student
+vikas - publish -event - .emit
+student -listen - on
+
+student - publish - event -emit
+vikas -listen -on
+
+script.js --student side
+index.js -vikas side 
+
+
+
+*/
+
+const btn=document.getElementById("sndmsg")
+var input =document.getElementById("inp")
+const msglist=document.getElementById("msglist")
+const vikasbtn=document.getElementById("vikasbtn")
 
 btn.onclick=function(){
- 
-        socket.emit("event-from-client")
-        const div2=document.createElement("div")
-        div2.innerText="msg coming from CLINET"
-        document.body.append(div2)
+    console.log("i am fired")
+    //publish
+
+    socket.emit('msg-from-students',{
+        msg:input.value     
+    })
+
+    const litem=document.createElement('li')
+    litem.innerText=input.value+"{from students}" ;
+    msglist.append(litem)
+
+  
 }
 
 
 
+vikasbtn.onclick=function(){
+    console.log("fired vikas button")
+
+    // socket.emit('msg-from-vikas')
+    socket.emit('msg-from-vikas');
+    
+    
 
 
-socket.on('event-from-server',(maal)=>{
-    console.log(maal)
+}
 
-    const div =document.createElement("div")
-    div.innerHTML="some thing coming from server"
-    document.body.append(div)
+socket.on('msg-from-student',(data)=>{
+
+    const litem = document.createElement('li');
+    litem.innerText = data.msg + " {from student}";
+    msglist.append(litem);
 
 })
+socket.on('msg-from-vikas',(data)=>{
 
+    const litem = document.createElement('li');
+    litem.innerText = data.msg + " {from vikas}";
+    msglist.append(litem);
+
+})
 
--- Chat Application Using Socketio/index.js
+++ Chat Application Using Socketio/index.js
@@ -11,7 +11,7 @@ app.use(express.static(path.join(__dirname, 'FE')));
 app.get('/',(req,res)=>{
     res.send("learning real time full suplex chat application using socket io built on websocket protocols")
 })
-server.listen(5000,()=>{
+server.listen(3000,()=>{
     console.log("server started ")
 
 
@@ -25,11 +25,78 @@ server.listen(5000,()=>{
 })
 
 
-io.on('connection',(socket)=>{
-    console.log("user connected")
+// io.on('connection',(socket)=>{
+//     console.log("user connected")
 
-    setInterval(() => {
-        socket.emit("event-from-server")
+//     // setInterval(() => {
+//     //     socket.emit("event-from-server")//publish
         
-    }, 2000);
+//     // }, 2000);
+
+
+//     // setInterval(() => {
+//     //     socket.emit("vikas-msg-event")//publish
+//     //     console.log("msg from vikas side ")
+
+
+        
+//     // }, 5000);
+
+//     socket.emit('dice-msg-side',{
+//         msg:"my first msg to you"
+//     })
+
+// })
+
+
+//listen
+
+// io.on("msg-from-student",(data)=>{
+//     console.log("student side msg",data)
+
+//     const div=document.createElement("div")
+//     div.innerText="msg from student side "
+//     document.body.append(div)
+
+
+// })
+
+
+
+
+
+
+// io.on('connection',()=>{
+//     console.log("user connected")
+
+//     //listen - student lki side wala event 
+
+   
+// })
+
+// io.on('msg-from-student',(data)=>{
+//     console.log("coming data",data)
+//     const div=document.createElement("div")
+//     div.innerText="hellooo"
+//     document.body.append(div)
+
+// })
+
+
+io.on('connection',(socket)=>{
+    console.log("user connected ")
+
+    socket.on('msg-from-students',(data)=>{
+        console.log("hey")
+        console.log(data)
+
+    })
+
+  socket.on('msg-from-vikas',(data)=>{
+    io.emit('msg-from-vikas', {
+        msg: "Hello from Vikas",
+    });
+
+  })
 })
+
